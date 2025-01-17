de26223902b4b1fcbcf73b847a0dbb0f8ea609fa
Sebastian Jeltsch <sebastian@trailbase.io>
2025-01-17 15:17:19 +0100

    Make rust client tests hermetic.

--- client/trailbase-rs/src/lib.rs
+++ client/trailbase-rs/src/lib.rs
@@ -10,12 +10,6 @@ use thiserror::Error;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 
-// TODO:
-//
-//  * Hermetic test setup (right now server needs to be running), will fail CI.
-//  * Fewer allocations
-//  * Fewer format!
-
 #[derive(Debug, Error)]
 pub enum Error {
   #[error("Reqwest: {0}")]
@@ -561,139 +555,22 @@ const RECORD_API: &str = "api/records/v1";
 #[cfg(test)]
 mod tests {
   use super::*;
-  use crate::Client;
-
-  use serde_json::json;
-
-  #[derive(Debug, Clone, Deserialize, Serialize)]
-  struct SimpleStrict {
-    id: String,
-
-    text_null: Option<String>,
-    text_default: Option<String>,
-    text_not_null: String,
-  }
-
-  async fn connect() -> Client {
-    let client = Client::new("http://127.0.0.1:4000", None);
-    let _ = client.login("admin@localhost", "secret").await.unwrap();
-    return client;
-  }
 
   #[tokio::test]
   async fn is_send_test() {
-    let client = connect().await;
-    let api = client.records("simple_strict_table");
-    tokio::spawn(async move {
-      // This would not compile if locks would be held across async function calls.
-      let response = api.read::<SimpleStrict>(0).await;
-      assert!(response.is_err());
-    })
-    .await
-    .unwrap();
-  }
+    let client = Client::new("http://127.0.0.1:4000", None);
 
-  #[tokio::test]
-  async fn login_test() {
-    let client = connect().await;
-
-    let tokens = client.tokens().unwrap();
-
-    assert_ne!(tokens.auth_token, "");
-    assert!(tokens.refresh_token.is_some());
-
-    let user = client.user().unwrap();
-    assert_eq!(user.email, "admin@localhost");
-
-    client.refresh().await.unwrap();
-
-    client.logout().await.unwrap();
-    assert!(client.tokens().is_none());
-  }
-
-  #[tokio::test]
-  async fn records_test() {
-    let client = connect().await;
     let api = client.records("simple_strict_table");
 
-    let now = now();
-
-    let messages = vec![
-      format!("rust client test 0: =?&{now}"),
-      format!("rust client test 1: =?&{now}"),
-    ];
-
-    let mut ids = vec![];
-    for msg in messages.iter() {
-      ids.push(api.create(json!({"text_not_null": msg})).await.unwrap());
-    }
-
-    {
-      // List one specific message.
-      let filter = format!("text_not_null={}", messages[0]);
-      let filters = vec![filter.as_str()];
-      let records = api
-        .list::<serde_json::Value>(None, None, Some(filters.as_slice()))
-        .await
-        .unwrap();
-
-      assert_eq!(records.len(), 1);
-    }
-
-    {
-      // List all the messages
-      let filter = format!("text_not_null[like]=% =?&{now}");
-      let records_ascending: Vec<SimpleStrict> = api
-        .list(None, Some(&["+text_not_null"]), Some(&[&filter]))
-        .await
-        .unwrap();
-
-      let messages_ascending: Vec<_> = records_ascending
-        .into_iter()
-        .map(|s| s.text_not_null)
-        .collect();
-      assert_eq!(messages, messages_ascending);
-
-      let records_descending: Vec<SimpleStrict> = api
-        .list(None, Some(&["-text_not_null"]), Some(&[&filter]))
-        .await
-        .unwrap();
-
-      let messages_descending: Vec<_> = records_descending
-        .into_iter()
-        .map(|s| s.text_not_null)
-        .collect();
-      assert_eq!(
-        messages,
-        messages_descending.into_iter().rev().collect::<Vec<_>>()
-      );
-    }
-
-    {
-      // Read
-      let record: SimpleStrict = api.read(&ids[0]).await.unwrap();
-      assert_eq!(ids[0], record.id);
-      assert_eq!(record.text_not_null, messages[0]);
-    }
-
-    {
-      // Update
-      let updated_message = format!("rust client updated test 0: {now}");
-      api
-        .update(&ids[0], json!({"text_not_null": updated_message}))
-        .await
-        .unwrap();
-
-      let record: SimpleStrict = api.read(&ids[0]).await.unwrap();
-      assert_eq!(record.text_not_null, updated_message);
-    }
-
-    {
-      // Delete
-      api.delete(&ids[0]).await.unwrap();
-
-      let response = api.read::<SimpleStrict>(&ids[0]).await;
-      assert!(response.is_err());
+    for _ in 0..2 {
+      let api = api.clone();
+      tokio::spawn(async move {
+        // This would not compile if locks would be held across async function calls.
+        let response = api.read::<serde_json::Value>(0).await;
+        assert!(response.is_err());
+      })
+      .await
+      .unwrap();
     }
   }
 }
